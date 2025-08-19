d3b4dbdc6b9bb1d56fe0de46f2787120b3ab20b4
ilyahub <ilay83@yandex.ru>
2025-08-19 19:04:44 +0500

    Update config.js

--- assets/app/config.js
+++ assets/app/config.js
@@ -1,8 +1,9 @@
-// assets/app/config.js
-export const DEAL_FIELD_CODE = 'UF_CRM_1755533553';  // множественное поле связей в сделке
-export const SMART_ENTITY_TYPE_ID = 1032;            // ID вашего смарт-процесса
+// Константы проекта (SPA "Лицензии")
+export const DEAL_FIELD_CODE = 'UF_CRM_1755533553';   // множественное поле связей/ID в сделке
+export const SMART_ENTITY_TYPE_ID = 1032;             // entityTypeId смарт-процесса
+export const PORTAL_ORIGIN = 'https://tehprof.bitrix24.kz';
 
-// UF-поля смарт-процесса «Лицензии»
+// UF-поля смарт-процесса
 export const F = {
   dealIdSource: 'UF_CRM_10_1717328665682', // ID исходной сделки (number)
   licenseKey  : 'UF_CRM_10_1717328730625', // Лицензионный ключ (string)
@@ -11,4 +12,25 @@ export const F = {
   tariffEnd   : 'UF_CRM_10_1717329087589', // Дата окончания тарифа (date)
   marketEnd   : 'UF_CRM_10_1717329109963', // Дата окончания подписки (date)
   product     : 'UF_CRM_10_1717329453779', // Продукт (list)
+};
+
+// Метки столбцов и порядок
+export const COL_ORDER = [
+  'id', 'title', 'ass', 'stage',
+  'deal', 'key', 'url', 'tariff', 'tEnd', 'mEnd', 'product', 'act'
+];
+
+export const COL_LABEL = {
+  id:'ID',
+  title:'Название',
+  ass:'Ответственный',
+  stage:'Стадия',
+  deal:'ID исходной сделки',
+  key:'Лицензионный ключ',
+  url:'Адрес портала',
+  tariff:'Текущий тариф',
+  tEnd:'Окончание тарифа',
+  mEnd:'Окончание подписки',
+  product:'Продукт',
+  act:'Действия'
 };
