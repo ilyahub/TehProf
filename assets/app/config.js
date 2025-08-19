0879ff15d4eed1c069794eaf0ce6bc1102e94e14
ilyahub <ilay83@yandex.ru>
2025-08-19 13:43:17 +0500

    Create config.js

---
+++ assets/app/config.js
@@ -0,0 +1,14 @@
+export const CONFIG = {
+  DEAL_FIELD_CODE: 'UF_CRM_1755533553',
+  SMART_ENTITY_TYPE_ID: 1032,
+  PORTAL_ORIGIN: 'https://tehprof.bitrix24.kz',
+  F: {
+    dealIdSource: 'UF_CRM_10_1717328665682',
+    licenseKey  : 'UF_CRM_10_1717328730625',
+    portalUrl   : 'UF_CRM_10_1717328814784',
+    tariff      : 'UF_CRM_10_1717329015552',
+    tariffEnd   : 'UF_CRM_10_1717329087589',
+    marketEnd   : 'UF_CRM_10_1717329109963',
+    product     : 'UF_CRM_10_1717329453779',
+  },
+};
